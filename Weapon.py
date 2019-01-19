a19d2e82618203a1bc3a2db90adabd6d3478b12b
LeniviyPaket <vsevolod.savchenko@inbox.ru>
2019-01-19 19:08:39 +0300

    Add files via upload

--- Weapon.py
+++ Weapon.py
@@ -1,8 +1,12 @@
 class weapon():
     #some variables
+    wpntype = None #0 if melee, 1 else
     ammo = None #-1 if melee, 0 or more else
     damage = None
     sprite = None
-    atk_distance = None #0 or more if melee, -1 else
+    atk_distance = None #1 or more if melee, -1 else
     
     description = "There must be <item> description. But now there is some sample text. :c"
+    
+    def usage():
+        pass
