569ba05c41a5bab5cb3b32169a87511e97bd25a3
LeniviyPaket <vsevolod.savchenko@inbox.ru>
2019-01-19 20:06:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import tkinter
+from PIL import ImageTk
+from PIL import Image
 from tkinter import messagebox
-from tkinter import *
 from random import *
 
 #importing some mechanics
@@ -11,12 +13,33 @@ from Weapon import *
 
 height_f, width_f, cellsize = [16, 12, 64]
 
-root = Tk()
-canvas = Canvas(root, width = cellsize * height_f + cellsize * 2, height = cellsize * width_f)
+
+field = []
+with open("polygon.txt", "r") as f:
+    for line in f:
+        field.append(list(line))
+
+
+root = tkinter.Tk()
+canvas = tkinter.Canvas(root, width = cellsize * height_f, height = cellsize * width_f)
+
+for i in range(width_f):
+    for j in range(height_f):
+        if field[i][j] == '#':
+            img = ImageTk.PhotoImage(Image.open(os.getcwd() + '/sprites/g_wall_base.png'))
+            tkinter.Label(root, image=img).pack()
+        else:
+            img = ImageTk.PhotoImage(Image.open(os.getcwd() + '/sprites/g_floor_base.png'))
+            tkinter.Label(root, image=img).pack()
+            
 
 #canvas.create_rectangle(0, 0, cellsize * height_f, cellsize * width_f, fill = 'red')
 
-#здесь скоро будут бинды
+##здесь скоро будут бинды
 
+#img = ImageTk.PhotoImage(Image.open(os.getcwd() + '/sprites/hl_weapon.png'))
+#view = tkinter.Label(root, padx = 100, pady = 100, image=img)
+#view.pack()
+#view.pack(side="center", fill="both", expand="yes")
 canvas.pack()
 root.mainloop()
